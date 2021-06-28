63462df43c98ee0c7e4fda43eac01639b7b85278
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2021-06-27 23:21:00 -0500

    Laboratorio_05

--- SpringBootDemo/src/main/java/com/makotojava/learn/springboot/SpringBootDemoController.java
+++ SpringBootDemo/src/main/java/com/makotojava/learn/springboot/SpringBootDemoController.java
@@ -1,11 +1,12 @@
 package com.makotojava.learn.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.makotojava.learn.odot.service.CategoryService;
 import com.makotojava.learn.odot.service.ItemService;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class SpringBootDemoController {
 
